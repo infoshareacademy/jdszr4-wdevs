80c23d8e8210a939af5999188ae2d793bae83a4e
Agnieszka Tetla <agnieszka.tetla@gmail.com>
2021-06-10 20:54:23 +0200

    Poprawiona sekcja Gross intake ratio in first grade of primary education - analysis

--- Education-analysis.sql
+++ Education-analysis.sql
@@ -1,67 +1,108 @@
 /*1)	Gross intake ratio in first grade of primary education, female (% of relevant age group)*/
-create temp table Gross_intake_1grade_female
-as
-select 	distinct i.countryname
-,		i."Year" 
-,		i.value
-,		round(lag(i.value) over (partition by i.countryname order by i."Year")::numeric,2) previous_year
-,		round(((i.value - lag(i.value) over (partition by i.countryname order by i."Year"))/
-		lag(i.value) over (partition by i.countryname order by i."Year"))::numeric,2) YoY_value
-from indicators i
-join country c on i.countrycode = c.countrycode 
-where c.region is not null and c.region <> '' and indicatorcode = 'SE.PRM.GINT.FE.ZS'
-group by i.countryname, i."Year", i.value;
+CREATE TEMP TABLE Gross_intake_1grade_female AS
+SELECT DISTINCT i.countryname,
+  i."Year",
+  i.value,
+  round(LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year")::NUMERIC, 2) previous_year,
+  round(((i.value - LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year"))/ LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year"))::NUMERIC, 2) YoY_value
+FROM
+  indicators i
+JOIN country c ON 
+  i.countrycode = c.countrycode
+WHERE
+  c.region IS NOT NULL
+  AND c.region <> ''
+  AND indicatorcode = 'SE.PRM.GINT.FE.ZS'
+GROUP BY
+  i.countryname,
+  i."Year",
+  i.value;
 
-select * from Gross_intake_1grade_female;
+SELECT
+  *
+FROM Gross_intake_1grade_female;
 
 /*2)	Gross intake ratio in first grade of primary education, male (% of relevant age group)*/
-create temp table Gross_intake_1grade_male
-as
-select 	distinct i.countryname
-,		i."Year" 
-,		i.value
-,		round(lag(i.value) over (partition by i.countryname order by i."Year")::numeric,2) previous_year
-,		round(((i.value - lag(i.value) over (partition by i.countryname order by i."Year"))/
-lag(i.value) over (partition by i.countryname order by i."Year"))::numeric,2) YoY_value
-from indicators i
-join country c on i.countrycode = c.countrycode 
-where c.region is not null and c.region <> '' and indicatorcode = 'SE.PRM.GINT.MA.ZS'
-group by i.countryname, i."Year", i.value;
+CREATE TEMP TABLE Gross_intake_1grade_male AS
+SELECT
+  DISTINCT i.countryname,
+  i."Year",
+  i.value,
+  round(LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year")::NUMERIC, 2) previous_year,
+  round(((i.value - LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year"))/ LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year"))::NUMERIC, 2) YoY_value
+FROM
+  indicators i
+JOIN country c ON
+  i.countrycode = c.countrycode
+WHERE
+  c.region IS NOT NULL
+  AND c.region <> ''
+  AND indicatorcode = 'SE.PRM.GINT.MA.ZS'
+GROUP BY
+  i.countryname,
+  i."Year",
+  i.value;
 
-select * from Gross_intake_1grade_male;
+SELECT
+  *
+FROM
+  Gross_intake_1grade_male;
 
 /*3)	Gross intake ratio in first grade of primary education, total (% of relevant age group)
  * Porównanie wartoœci dla kobiet, mê¿czyzn oraz dla kobiet i mê¿czyzn ³¹cznie.
  * WskaŸnik reprezentuje stosunek osób przyjêtych do pierwszej klasy szko³y podstawowej do grupy dzieci w wieku szkolnym, 
  * które powinny rozpocz¹æ edukacjê w danym roku.
  */ 
-create temp table Gross_intake_1grade_total
-as
-select 	distinct i.countryname
-,		i."Year" 
-,		i.value
-,		round(lag(i.value) over (partition by i.countryname order by i."Year")::numeric,2) previous_year
-,		round(((i.value - lag(i.value) over (partition by i.countryname order by i."Year"))/
-lag(i.value) over (partition by i.countryname order by i."Year"))::numeric,2) YoY_value
-from indicators i
-join country c on i.countrycode = c.countrycode 
-where c.region is not null and c.region <> '' and indicatorcode = 'SE.PRM.GINT.ZS'
-group by i.countryname, i."Year", i.value;
+CREATE TEMP TABLE Gross_intake_1grade_total AS
+SELECT
+  DISTINCT i.countryname,
+  i."Year",
+  i.value,
+  round(LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year")::NUMERIC, 2) previous_year,
+  round(((i.value - LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year"))/ LAG(i.value) OVER (PARTITION BY i.countryname ORDER BY i."Year"))::NUMERIC, 2) YoY_value
+FROM
+  indicators i
+JOIN country c ON
+  i.countrycode = c.countrycode
+WHERE
+  c.region IS NOT NULL
+  AND c.region <> ''
+  AND indicatorcode = 'SE.PRM.GINT.ZS'
+GROUP BY
+  i.countryname,
+  i."Year",
+  i.value;
 
 
-select * from Gross_intake_1grade_female;
-select * from Gross_intake_1grade_male;
-select * from Gross_intake_1grade_total;
+SELECT
+  *
+FROM
+  Gross_intake_1grade_female;
 
-select 
-		f.countryname 
-,		f."Year"
-,		round(f.value::numeric,2) as female
-,		round(m.value::numeric,2) as male
-,		round(t.value::numeric,2) as total_f_m
-from Gross_intake_1grade_female f
-join Gross_intake_1grade_male m on f.countryname = m.countryname and f."Year" = m."Year"
-join Gross_intake_1grade_total t on f.countryname = t.countryname and f."Year" = t."Year";
+SELECT
+  *
+FROM
+  Gross_intake_1grade_male;
+
+SELECT
+  *
+FROM
+  Gross_intake_1grade_total;
+
+SELECT
+  f.countryname,
+  f."Year",
+  round(f.value::NUMERIC, 2) AS female,
+  round(m.value::NUMERIC, 2) AS male,
+  round(t.value::NUMERIC, 2) AS total_f_m
+FROM
+  Gross_intake_1grade_female f
+JOIN Gross_intake_1grade_male m ON
+  f.countryname = m.countryname
+  AND f."Year" = m."Year"
+JOIN Gross_intake_1grade_total t ON
+  f.countryname = t.countryname
+  AND f."Year" = t."Year";
 
 
 
@@ -69,93 +110,134 @@ join Gross_intake_1grade_total t on f.countryname = t.countryname and f."Year" =
  * Poniewa¿ dla niektórych lat i krajów posiadane dane s¹ niekompletne, postanowiono dokonaæ analizy w dekadach
  */
 
-create table decades
-as
-select distinct i."Year"
-,		concat((i."Year"/10),'0') as decade_start_date
-,		concat((i."Year"/10)::varchar,'0')::numeric + 9 as decade_end_date
-,		substring(cast(i."Year" as varchar),3,1) as decade
-,		substring(cast(i."Year" as varchar),1,2)::numeric + 1 as century
-from indicators i
-order by i."Year";
+CREATE TABLE decades AS
+SELECT
+  DISTINCT i."Year",
+  concat((i."Year" / 10), '0') AS decade_start_date,
+  concat((i."Year" / 10)::varchar, '0')::NUMERIC + 9 AS decade_end_date,
+  substring(CAST(i."Year" AS varchar), 3, 1) AS decade,
+  substring(CAST(i."Year" AS varchar), 1, 2)::NUMERIC + 1 AS century
+FROM
+  indicators i
+ORDER BY
+  i."Year";
 
-select * from decades;
+SELECT
+  *
+FROM
+  decades;
 
 /* Gender gaps per dekada
  * Pokazuje ró¿nicê miêdzy przyjêciem do szko³y podstawowej kobiet i mê¿czyzn per rok i dekada.*/
 
-create temp table Gender_gaps
-as
-select 
-		c.region
-,		f.countryname 
-,		d.century
-,		d.decade
-,		f."Year"
-,		f.value as female
-,		m.value as male
-,		m.value - f.value as gender_gap
-from Gross_intake_1grade_female f
-join Gross_intake_1grade_male m on f.countryname = m.countryname and f."Year" = m."Year"
-join Gross_intake_1grade_total t on f.countryname = t.countryname and f."Year" = t."Year"
-join country c on f.countryname = c.tablename
-join decades d on f."Year" = d."Year"
-group by c.region
-,		f.countryname 
-,		d.century 
-,		d.decade 
-,		f."Year"
-,		f.value
-,		m.value
-order by f.countryname, d.century, d.decade;
+CREATE TEMP TABLE Gender_gaps AS
+SELECT
+  c.region,
+  f.countryname,
+  d.century,
+  d.decade,
+  f."Year",
+  f.value AS female,
+  m.value AS male,
+  m.value - f.value AS gender_gap
+FROM
+  Gross_intake_1grade_female f
+JOIN Gross_intake_1grade_male m ON
+  f.countryname = m.countryname
+  AND f."Year" = m."Year"
+JOIN Gross_intake_1grade_total t ON
+  f.countryname = t.countryname
+  AND f."Year" = t."Year"
+JOIN country c ON
+  f.countryname = c.tablename
+JOIN decades d ON
+  f."Year" = d."Year"
+GROUP BY
+  c.region,
+  f.countryname,
+  d.century,
+  d.decade,
+  f."Year",
+  f.value,
+  m.value
+ORDER BY
+  f.countryname,
+  d.century,
+  d.decade;
 
 
-select * from gender_gaps gp;
+SELECT
+  *
+FROM
+  gender_gaps gp;
 /*where gp.gender_gap = gp.min_gender_gap or gp.gender_gap = gp.max_gender_gap*/
 
------------------------
-/*Podzia³ w dekadach*/
-/*select distinct
-		gp.region
-,		gp.countryname
-,		gp.century
-,		gp.decade
-,		min(gp.gender_gap) over (partition by gp.decade, gp.countryname)
-,		max(gp.gender_gap) over (partition by gp.decade, gp.countryname)
-from gender_gaps gp
-order by gp.countryname, gp.century, gp.decade;
 
-select * from gender_gaps;*/
-
------------------------------
 /*Pokazuje œredni¹, min oraz max gender gap per kraj oraz dekadê*/
 
-create temp table gender_gaps_basic_measures
-as
-select distinct
-		gp.region
-,		gp.countryname
-,		gp.century
-,		gp.decade
-,		avg(gp.gender_gap) over (partition by gp.decade, gp.countryname) as avg_gap_country_and_decade
-,		min(gp.gender_gap) over (partition by gp.decade, gp.countryname) as min_gap_country_and_decade
-,		max(gp.gender_gap) over (partition by gp.decade, gp.countryname) as max_gap_country_and_decade
-from gender_gaps gp
-order by gp.region, gp.century, gp.decade;
+CREATE TEMP TABLE gender_gaps_basic_measures AS
+SELECT
+  DISTINCT gp.region,
+  gp.countryname,
+  gp.century,
+  gp.decade,
+  avg(gp.gender_gap) OVER (
+    PARTITION BY gp.decade,
+    gp.countryname
+  ) AS avg_gap_country_and_decade,
+  min(gp.gender_gap) OVER (
+    PARTITION BY gp.decade,
+    gp.countryname
+  ) AS min_gap_country_and_decade,
+  max(gp.gender_gap) OVER (
+    PARTITION BY gp.decade,
+    gp.countryname
+  ) AS max_gap_country_and_decade
+FROM
+  gender_gaps gp
+ORDER BY
+  gp.region,
+  gp.century,
+  gp.decade;
 
-select * from gender_gaps_basic_measures
+SELECT
+  *
+FROM
+  gender_gaps_basic_measures;
 
-select 
-		gpm.region
-,		gpm.countryname
-,		gpm.century
-,		gpm.decade
-,		gpm.avg_gap_country_and_decade
-,		(select min(gpm.avg_gap_country_and_decade) from gender_gaps_basic_measures gpm)
-,		(select max(gpm.avg_gap_country_and_decade) from gender_gaps_basic_measures gpm)
-from gender_gaps_basic_measures gpm
-where gpm.avg_gap_country_and_decade = (select min(gpm.avg_gap_country_and_decade) from gender_gaps_basic_measures gpm)
-		or gpm.avg_gap_country_and_decade = (select max(gpm.avg_gap_country_and_decade) from gender_gaps_basic_measures gpm);
+SELECT
+  gpm.region,
+  gpm.countryname,
+  gpm.century,
+  gpm.decade,
+  gpm.avg_gap_country_and_decade,
+  (
+    SELECT
+      min(gpm.avg_gap_country_and_decade)
+    FROM
+      gender_gaps_basic_measures gpm
+  ) ,
+  (
+    SELECT
+      max(gpm.avg_gap_country_and_decade)
+    FROM
+      gender_gaps_basic_measures gpm
+  )
+FROM
+  gender_gaps_basic_measures gpm
+WHERE
+  gpm.avg_gap_country_and_decade = (
+    SELECT
+      min(gpm.avg_gap_country_and_decade)
+    FROM
+      gender_gaps_basic_measures gpm
+  )
+  OR gpm.avg_gap_country_and_decade = (
+    SELECT
+      max(gpm.avg_gap_country_and_decade)
+    FROM
+      gender_gaps_basic_measures gpm
+  );
 
 /* Na przestrzeni lat, najwiêksza dysproporcja ze wzglêdu na p³eæ wyst¹pi³a w Bhutanie oraz Lesotho w latach 70tych ubieg³ego wieku.
  * W Buthanie przyjêto do pierwszej klasy o 64 pkt procentowe wiêcej ch³opców ni¿ dziewczynech, 
@@ -165,106 +247,163 @@ where gpm.avg_gap_country_and_decade = (select min(gpm.avg_gap_country_and_decad
 
 /*Pokazuje œredni¹ gender gap per kraj oraz wiek*/
 
-create temp table gender_gaps_basic_measures2
-as
-select distinct
-		gp.region
-,		gp.countryname
-,		gp.century
-,		gp.decade
-,		avg(gp.gender_gap) over (partition by gp.decade, gp.countryname) as avg_gap_country_and_decade
-from gender_gaps gp
-order by gp.region, gp.century, gp.decade;
+CREATE TEMP TABLE gender_gaps_basic_measures2 AS
+SELECT
+  DISTINCT gp.region,
+  gp.countryname,
+  gp.century,
+  gp.decade,
+  avg(gp.gender_gap) OVER (
+    PARTITION BY gp.decade,
+    gp.countryname
+  ) AS avg_gap_country_and_decade
+FROM
+  gender_gaps gp
+ORDER BY
+  gp.region,
+  gp.century,
+  gp.decade;
 
-select * from gender_gaps_basic_measures2;
+SELECT
+  *
+FROM
+  gender_gaps_basic_measures2;
 
 /*ranking per century i decade - top 3
  * Pokazuje ranking per kraj i dekada w obrêbie regionu - wartoœæ bezwzglêdna - im bli¿ej zera tym kraj bardziej rozwiniêty */
-create temp table ranking_top_3
-as
-select gpm2.region, gpm2.countryname, gpm2.century, gpm2.decade, abs(gpm2.avg_gap_country_and_decade) as wartosc_bezwzgledna
-,	dense_rank() over(partition by gpm2.region, gpm2.century, gpm2.decade order by abs(gpm2.avg_gap_country_and_decade))  as ranking
-,	count(*) over (partition by gpm2.countryname) liczba_wystapien
-from gender_gaps_basic_measures2 gpm2;
+CREATE TEMP TABLE ranking_top_3 AS
+SELECT
+  gpm2.region,
+  gpm2.countryname,
+  gpm2.century,
+  gpm2.decade,
+  abs(gpm2.avg_gap_country_and_decade) AS wartosc_bezwzgledna,
+  DENSE_RANK() OVER(
+    PARTITION BY gpm2.region,
+    gpm2.century,
+    gpm2.decade
+  ORDER BY
+    abs(gpm2.avg_gap_country_and_decade)
+  ) AS ranking,
+  count(*) OVER (
+    PARTITION BY gpm2.countryname
+  ) liczba_wystapien
+FROM
+  gender_gaps_basic_measures2 gpm2;
 
-select * from ranking_top_3;
+SELECT
+  *
+FROM
+  ranking_top_3;
 
-select *
-from ranking_top_3 r3
-where r3.ranking <= 3;
+SELECT
+  *
+FROM
+  ranking_top_3 r3
+WHERE
+  r3.ranking <= 3;
 
 -----------------------------------
 /*sytuacja krajów dla których mamy dane conajmniej dla 3 dekad*/
 
-select 
-		r3.region
-,		r3.countryname
-,		r3.countryname
-,		r3.century
-,		r3.decade
-,		r3.wartosc_bezwzgledna
-,		r3.ranking
-from ranking_top_3 r3
-where r3.liczba_wystapien >= '3'
-group by r3.region, r3.countryname, r3.century, r3.decade, r3.wartosc_bezwzgledna, r3.ranking, r3.liczba_wystapien
-order by r3.region, r3.countryname, r3.century, r3.decade;
-
---Punkty
-/*select 
-		r3.region
-,		r3.countryname
-,		r3.century
-,		r3.decade
-,		r3.wartosc_bezwzgledna
-,		r3.ranking
-,		avg(r3.ranking) over (partition by r3.countryname, r3.century)
-from ranking_top_3 r3
-where r3.liczba_wystapien >= '3'
-group by r3.region, r3.countryname, r3.century, r3.decade, r3.wartosc_bezwzgledna, r3.ranking, r3.liczba_wystapien
-order by r3.region, r3.countryname, r3.century, r3.decade;*/
-
+SELECT
+  r3.region,
+  r3.countryname,
+  r3.countryname,
+  r3.century,
+  r3.decade,
+  r3.wartosc_bezwzgledna,
+  r3.ranking
+FROM
+  ranking_top_3 r3
+WHERE
+  r3.liczba_wystapien >= '3'
+GROUP BY
+  r3.region,
+  r3.countryname,
+  r3.century,
+  r3.decade,
+  r3.wartosc_bezwzgledna,
+  r3.ranking,
+  r3.liczba_wystapien
+ORDER BY
+  r3.region,
+  r3.countryname,
+  r3.century,
+  r3.decade;
 
 /* Analiza poni¿ej pokazuje ruchy w rankingu na przestrzeni dwóch ostatnich wieków - wielkoœæ gender gap. 
  * Pokazuje wzmocnienie lub os³abienie danego kraju w obrêbie regionu.
  * Ranking nie bie¿e pod uwagê ró¿nic w wielkoœci populacji w poszczególnych krajach*/
  /* Tabela Punkty_20 pokazuje œrednie miejsce w rankinu w 20 wieku per kraj w regionie - dla wykazania tendencji*/
 
-create temp table Punkty_20
-as
-select distinct
-		r3.region
-,		r3.countryname
-,		r3.century
-,		avg(r3.ranking) over (partition by r3.countryname, r3.century) as avg_ranking_20_century
-from ranking_top_3 r3
-where r3.liczba_wystapien >= '3' and r3.century = 20
-group by r3.region, r3.countryname, r3.century, r3.ranking
-order by r3.region, r3.countryname, r3.century;
+CREATE TEMP TABLE Punkty_20 AS
+SELECT
+  DISTINCT r3.region,
+  r3.countryname,
+  r3.century,
+  avg(r3.ranking) OVER (
+    PARTITION BY r3.countryname,
+    r3.century
+  ) AS avg_ranking_20_century
+FROM
+  ranking_top_3 r3
+WHERE
+  r3.liczba_wystapien >= '3'
+  AND r3.century = 20
+GROUP BY
+  r3.region,
+  r3.countryname,
+  r3.century,
+  r3.ranking
+ORDER BY
+  r3.region,
+  r3.countryname,
+  r3.century;
 
-select * from Punkty_20;
+SELECT
+  *
+FROM
+  Punkty_20;
 
 /*Tabela Punkty_21 pokazuje œrednie miejsce w rankinu w 21 wieku per kraj w regionie - dla wykazania tendencji*/
-create temp table Punkty_21
-as
-select distinct
-		r3.region
-,		r3.countryname
-,		r3.century
-,		avg(r3.ranking) over (partition by r3.countryname, r3.century) as avg_ranking_21_century
-from ranking_top_3 r3
-where r3.liczba_wystapien >= '3' and r3.century = 21
-group by r3.region, r3.countryname, r3.century, r3.ranking
-order by r3.region, r3.countryname, r3.century;
+CREATE TEMP TABLE Punkty_21 AS
+SELECT
+  DISTINCT r3.region,
+  r3.countryname,
+  r3.century,
+  avg(r3.ranking) OVER (
+    PARTITION BY r3.countryname,
+    r3.century
+  ) AS avg_ranking_21_century
+FROM
+  ranking_top_3 r3
+WHERE
+  r3.liczba_wystapien >= '3'
+  AND r3.century = 21
+GROUP BY
+  r3.region,
+  r3.countryname,
+  r3.century,
+  r3.ranking
+ORDER BY
+  r3.region,
+  r3.countryname,
+  r3.century;
 
-select 
-	p20.region
-,	p20.countryname
-,	round(p20.avg_ranking_20_century,2) as century_20
-,	round(p21.avg_ranking_21_century,2) as century_21
-,	round((p20.avg_ranking_20_century - p21.avg_ranking_21_century),2) as progress
-from Punkty_20 p20
-join punkty_21 p21 on p20.countryname = p21.countryname
-order by p20.region, round((p20.avg_ranking_20_century - p21.avg_ranking_21_century),2);
+SELECT
+  p20.region,
+  p20.countryname,
+  round(p20.avg_ranking_20_century, 2) AS century_20,
+  round(p21.avg_ranking_21_century, 2) AS century_21,
+  round((p20.avg_ranking_20_century - p21.avg_ranking_21_century), 2) AS progress
+FROM
+  Punkty_20 p20
+JOIN punkty_21 p21 ON
+  p20.countryname = p21.countryname
+ORDER BY
+  p20.region,
+  round((p20.avg_ranking_20_century - p21.avg_ranking_21_century), 2);
 
 /* East Asia & Pacific: najwiêkszy spadek wyst¹pi³ w Malezji, 
  * w miarê podobne tempo rozmowu maj¹ Tajlandia, Tonga, Mongolia, Myanmar oraz Filipiny.
